12b22853b9c66effbb5c312fea4f8000d1ee1488
artur0527rg <rogozinskiyartur@gmail.com>
2024-01-09 17:10:17 +0300

    Start/help commands keyboard

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -2,9 +2,11 @@ from aiogram import Bot
 from aiogram.filters import CommandObject
 from aiogram.types import Message
 
+from core.keyboards import inline
+
 async def send_welcome(message: Message, bot:Bot):
     msg = "<b>Bing Chat Bot</b>\n"\
         "/help - Show help message\n"\
         "/reset - Reset conversation\n"\
         "/switch - Switch conversation style (creative,balanced,precise)\n"
-    await message.answer(msg)
+    await message.answer(msg, reply_markup=inline.links_markup)
---
+++ core/keyboards/inline.py
@@ -0,0 +1,16 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+links_markup = InlineKeyboardMarkup(inline_keyboard=[
+  [
+    InlineKeyboardButton(
+      text='Github',
+      url='https://github.com/artur0527rg/BingChatBot',
+    )
+  ],
+  [
+    InlineKeyboardButton(
+      text='Github(owner)',
+      url='https://github.com/pininkara/BingChatBot',
+    )
+  ],
+])
